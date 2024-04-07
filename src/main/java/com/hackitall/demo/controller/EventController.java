f19a448db85f871eb87de931608122791c69dbaa
MarioBujor2001 <bujormario2001@gmail.com>
2024-04-07 03:24:36 +0300

    One more toggle ... 3:24 am

--- src/main/java/com/hackitall/demo/controller/EventController.java
+++ src/main/java/com/hackitall/demo/controller/EventController.java
@@ -2,6 +2,7 @@ package com.hackitall.demo.controller;
 
 import com.hackitall.demo.mock.MockCollection;
 import com.hackitall.demo.model.EnergyEventDAO;
+import com.hackitall.demo.model.ToggleDTO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,19 +10,20 @@ import java.util.Random;
 
 @RestController
 @RequestMapping("/events")
+@CrossOrigin(origins = "*")
 public class EventController {
     @PutMapping("/{email}/{name}")
     public void updateStatus(@PathVariable("email") String email,
                              @PathVariable("name") String name,
-                             @RequestBody Boolean status){
+                             @RequestBody ToggleDTO toggle){
         EnergyEventDAO event = MockCollection.mocks
                 .entrySet()
                 .stream()
                 .filter(e -> e.getValue().getEmail().equals(email) && e.getValue().getName().equals(name))
                 .findFirst()
                 .get().getValue();
-        event.setStatus(status);
-        if(!status){
+        event.setStatus(toggle.getStatus());
+        if(!toggle.getStatus()){
             event.setConsumption(0f);
         }else{
             if (event.getConsumption() == 0f){
---
+++ src/main/java/com/hackitall/demo/model/ToggleDTO.java
@@ -0,0 +1,10 @@
+package com.hackitall.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ToggleDTO {
+    private Boolean status;
+}
